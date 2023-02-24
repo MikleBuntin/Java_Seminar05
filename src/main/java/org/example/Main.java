3636aef9cd6e3801fd82c5cce9fddb99733befec
MikleBuntin <BuntinMA@mail.ru>
2023-02-24 11:42:58 +0300

    Задача 01. Hashmap

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,27 @@
 package org.example;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        Map<Integer, String> names = new HashMap<>();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Введите номер Имя");
+            String[] name = iScanner.nextLine().split(" "); // name[0] - number, name[1] - name.
+            names.put(Integer.parseInt(name[0]), name[1]);
+        }
+        for (var el : names.entrySet()) {
+            if (el.getValue().equals("Иванов")) {
+                System.out.print(el.getKey() + ": " + el.getValue() + "\n");
+            }
 
-        System.out.println("Hello world!");
+
+        }
+        iScanner.close();
     }
+
 }
